0cb78702c8de8a7cb589a92c10ccce347e51effd
moenguyenx <qminh1908@gmail.com>
2024-10-25 21:39:22 +0700

    hehe added some chart

--- WebApp/weather_station/app/chart/page.jsx
+++ WebApp/weather_station/app/chart/page.jsx
@@ -1,25 +1,61 @@
 "use client"
 import React from "react";
-import Image from "next/image";
-import WeatherWidget from "@/components/widget";
 import Navbar from "@/components/navbar";
-import {
-	MDBCard,
-	MDBCardBody,
-	MDBCol,
-	MDBContainer,
-	MDBIcon,
-	MDBRow,
-	MDBTypography,
-  } from "mdb-react-ui-kit";
+import dynamic from 'next/dynamic';
+import 'chart.js/auto';
+const Line = dynamic(() => import('react-chartjs-2').then((mod) => mod.Line), {
+  ssr: false,
+});
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 export default function Chart() {
-  return (
-    <>
-        <Navbar></Navbar>
-        <h1>Hello</h1>
-    </>
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May'],
+    datasets: [
+      {
+        label: 'Line Chart',
+        data: [65, 59, 80, 81, 56],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+    ],
+  };
 
+  const options = {
+    plugins: {
+      legend: {
+        labels: {
+          color: 'white', // Set legend text color to white
+        },
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: 'white', // Set x-axis text color to white
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.2)', // Light white grid color
+        },
+      },
+      y: {
+        ticks: {
+          color: 'white', // Set y-axis text color to white
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.2)', // Light white grid color
+        },
+      },
+    },
+  };
+  return (
+      <section className="vh-100" style={{ backgroundColor: "#4B515D" }}>
+        <Navbar></Navbar>
+        <div style={{ width: '700px', height: '700px', color: 'white', margin: 'auto', textAlign: 'center' }}>
+          <h1>Example 1: Line Chart</h1>
+          <Line data={data} options={options}/>
+        </div>
+      </section>
   );
 }
--- WebApp/weather_station/app/page.jsx
+++ WebApp/weather_station/app/page.jsx
@@ -3,33 +3,50 @@ import React from "react";
 import WeatherWidget from "@/components/widget";
 import Navbar from "@/components/navbar";
 import {
-	MDBCard,
-	MDBCardBody,
-	MDBCol,
 	MDBContainer,
-	MDBIcon,
 	MDBRow,
-	MDBTypography,
   } from "mdb-react-ui-kit";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 export default function Home() {
+	const weatherData = [
+		{
+		  location: "Long Bien",
+		  temp: 13,
+		  humid: 84,
+		  time: "15:07",
+		  raining: 1
+		},
+		{
+		  location: "Hai Ba Trung",
+		  temp: 18,
+		  humid: 70,
+		  time: "12:30",
+		  raining: 0
+		},
+		{
+			location: "Ha Dong",
+			temp: 18,
+			humid: 70,
+			time: "12:30",
+			raining: 0
+		}
+	  ];
   return (
     <section className="vh-100" style={{ backgroundColor: "#4B515D" }}>
 		<Navbar></Navbar>
           
 		<MDBContainer className="h-100">
         	<MDBRow className="justify-content-center align-items-center h-100">
-				<WeatherWidget location="Long Bien" 
-								temp={37}
-								humid={90}
-								raining={1}
-								pm25={190}
-								feellikeTemp={35}
-								time="22:00"
-								/>
-				<WeatherWidget location="Hai Ba Trung" temp={38}></WeatherWidget>
-				<WeatherWidget location="Ha Dong" temp={39}></WeatherWidget>
+				{weatherData.map((data, index) => (
+            	<WeatherWidget
+            	  key={index}
+            	  location={data.location}
+            	  temp={data.temp}
+            	  humid={data.humid}
+            	  time={data.time}
+            	  raining={data.raining}
+            	/>))}
 			</MDBRow>
 		</MDBContainer>
 	</section>
